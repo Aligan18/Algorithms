ab1295725705f1cf2317422657a0503cefc81ca4
Aligan18 <alisherganiev18@gmail.com>
2023-05-09 18:36:55 +0600

    I solved the problem  number 35_Search_Insert_Position on leetcode

--- src/Data types/PrimitiveData.ts
+++ src/Data types/PrimitiveData.ts
@@ -1,12 +1,13 @@
-typeof "hello" //?   string
+//Primitive Data 
+typeof "hello"//?    string  
 typeof Symbol() //?  symbol
 typeof 1 //?         number
 typeof 123n //?      bigint
 typeof true //?      boolean
 typeof undefined //? undefined
+typeof null //?      object
 ////////////////////////////////////////////////////////////////////////////////////////////////
 typeof {} //?        object
-typeof null //?      object
 typeof alert //?     function
 
 
---
+++ src/binary search/35_Search_Insert_Position.ts
@@ -0,0 +1,34 @@
+//Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+function searchInsert(nums: number[], target: number): number {
+
+    if (nums[0] > target) {return 0}
+    if (nums[nums.length] < target) {return nums.length}
+
+    let  start = 0
+    let end = nums.length
+    let finished = false //?
+    let  position = 0
+    let cash =-1
+    let mid 
+    
+    while(mid !== cash){
+        cash = mid
+        mid =  Math.floor((start + end)/2)
+
+        if(nums[mid] === target) {
+            finished = true
+            position =mid
+            return position
+        }
+        else if(nums[mid] > target) {
+            end  = mid
+        }
+        else if(nums[mid] < target) {
+            start  = mid
+        }
+    }
+    return cash +1
+};
+
+ console.log(searchInsert([1,3,5,6], 4) )//?
